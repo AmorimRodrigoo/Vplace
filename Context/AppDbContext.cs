31703414fc9d7cc7d5e1aa139ca854ab751b8610
Rodrigo Amorim <amorimrodrigo071@gmail.com>
2025-08-26 22:57:56 -0300

    Migrations and Database Seed

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductApi.Models;
+
+namespace ProductApi.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {}
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Products { get; set; }
+    
+    //Fluent Api
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //Category
+        modelBuilder.Entity<Category>().HasKey(c=> c.CategoryId);
+        
+        modelBuilder.Entity<Category>().
+            Property(c=> c.Name).
+            HasMaxLength(100).
+            IsRequired();
+        
+        //Product
+        modelBuilder.Entity<Product>().
+            Property(c=> c.Description).
+            HasMaxLength(100).
+            IsRequired();
+        
+        modelBuilder.Entity<Product>().
+            Property(c=> c.Name).
+            HasMaxLength(100).
+            IsRequired();
+        
+        modelBuilder.Entity<Product>().
+            Property(c=> c.ImageUrl).
+            HasMaxLength(255).
+            IsRequired();
+        
+        modelBuilder.Entity<Product>().
+            Property(c=> c.Price).
+            HasPrecision(12, 2).
+            IsRequired();
+        
+        modelBuilder.Entity<Category>().
+            HasMany(g=> g.Products).
+            WithOne(g=> g.Category).
+            IsRequired().
+            OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<Category>().HasData(
+            new Category
+            {
+                CategoryId = 1,
+                Name = "Material Escolar"
+            },
+            new Category
+            {
+                CategoryId = 2,
+                Name = "Acessórios"
+            });
+    }
+}
---
+++ Migrations/20250827012701_SeedProducts.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProductApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedProducts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "INSERT INTO Products (Name, Description, Price, Stock, ImageUrl, CategoryId) " +
+                "VALUES ('Caderno', 'Caderno Vermelho', 10.00, 10, 'caderno1.jpg', 1)");
+            
+            migrationBuilder.Sql(
+                "INSERT INTO Products (Name, Description, Price, Stock, ImageUrl, CategoryId) " +
+                "VALUES ('Caneta', 'Caneta Preta', 2.00, 20, 'caneta1.jpg', 1)");
+            
+            migrationBuilder.Sql(
+                "INSERT INTO Products (Name, Description, Price, Stock, ImageUrl, CategoryId) " +
+                "VALUES ('Chaveiro', 'Chaveiro Clássico', 12.00, 15, 'chaveiro1.jpg', 2)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "DELETE FROM Products WHERE Name IN ('Caderno', 'Caneta', 'Chaveiro')");
+        }
+    }
+}
